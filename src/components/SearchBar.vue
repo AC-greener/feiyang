2ec4ae9fe8b4aa4465ea02df11227fba8cddf492
zhangchaoming <2508525199@qq.com>
2020-08-31 14:13:46 +0800

    time format

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -113,8 +113,14 @@ export default {
       var h=today.getHours()
       var m=today.getMinutes()
       var s=today.getSeconds()
+      if (h < 10){
+        h = '0' + h
+      }
       if (m < 10) {
         m = '0' + m
+      }
+      if (s < 10){
+        s = '0' + s
       }
       this.now = h + ':' + m + ':' + s
     },
